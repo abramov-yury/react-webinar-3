0baf4149ec22269bcd57a3b74c2e16ae89ff53d6
yury <yury.message@gmail.com>
2023-05-11 22:15:55 +0400

    Добавить вспомогательные функции

--- src/utils.js
+++ src/utils.js
@@ -26,3 +26,36 @@ export function createElement(name, props = {}, ...children) {
 
   return element;
 }
+
+/**
+ * Генерация строки из случайных чисел и букв
+ */
+
+export function getStrRand () {
+  let result = '';
+  const words = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+  const maxPosition = words.length - 1;
+
+  for (let i = 0; i < 6; i++) {
+    let position = Math.floor(Math.random() * maxPosition);
+    result = result + words.substring(position, position + 1);
+  }
+
+  return result;
+}
+
+/**
+ * Добавление окончания к существительным
+ */
+
+export function makeEnding(number, prefix, one, two, many) {
+  const str = number.toString()
+  const digit = parseInt(str[str.length - 1], 10);
+  if (isNaN(digit)) {return ""}
+
+  if (str.length > 1 && str[str.length - 2] === '1') {return number + " " + prefix + many;}
+  if (digit === 1) {return number + " " + prefix + one;}
+  else if (digit > 1 && digit <= 4) {return number + " " + prefix + two;}
+  else if (digit === 0 || digit >= 5) {return number + " " + prefix + many;}
+  else {return ""}
+}
