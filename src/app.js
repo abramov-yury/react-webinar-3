bdd6d193460278d1c3b9aaa23bfb36ab85ce527c
yury <yury.message@gmail.com>
2023-05-18 17:18:58 +0400

    Подключить компонент page-layout

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import './styles.css';
 import Head from './components/head/index.js';
 import List from './components/list/index.js';
 import Controls from './components/controls/index.js';
+import PageLayout from './components/page-layout/index.js';
 
 /**
  * Приложение
@@ -16,17 +17,11 @@ function App({store}) {
   const list = store.getState().list;
 
   return (
-    <div className='App'>
-      <div className='App-head'>
-        <Head title='Приложение на чистом JS' />
-      </div>
-      <div className='App-controls'>
-        <Controls store={store} />
-      </div>
-      <div className='App-center'>
-        <List store={store} />
-      </div>
-    </div>
+    <PageLayout
+      head={<Head title='Приложение на чистом JS' />}
+      controls={<Controls store={store} />}
+      content={<List store={store} />}
+    />
   );
 }
 
