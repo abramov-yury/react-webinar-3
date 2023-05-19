4cdae8563b2bb446417aecb44086f69949e6cc24
yury <yury.message@gmail.com>
2023-05-19 18:17:43 +0400

    (cart) реализовать предварительную вёрстку

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -5,8 +5,12 @@ import './style.css';
 function Cart({goToCart}) {
   return (
     <div className="Cart">
+      <p className="Cart-info">
+        <span className="Cart-text">В корзине:</span>
+        <span className="Cart-data">пусто</span>
+      </p>
       <button
-        className="Cart__button"
+        className="Cart-button"
         onClick={() => goToCart()}>Перейти</button>
     </div>
   )
